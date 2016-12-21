589d6a0a0ee86480231c1605c031c55d482b14e2
Finn Gluecki <finngluecki@gmx.ch>
2016-12-21 17:34:05 +0100

    Day 2

--- Calendar/src/Calendar/edit.java
+++ Calendar/src/Calendar/edit.java
@@ -1,16 +1,55 @@
 package Calendar;
 
+import java.io.File;
+import javax.swing.JFileChooser;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class edit {
-	
-	public edit(){
 
-	}
 	@FXML
-	public void template(ActionEvent e) {
-		System.out.println("it works");
+	protected ImageView pic1;
+	@FXML
+	protected ImageView pic2;
+	
+	public edit() {
+		
 	}
+	
+	@FXML
+	public void upload(MouseEvent e){
+		JFileChooser choose = new JFileChooser();
+		choose.showOpenDialog(null);
+		File img = choose.getSelectedFile();
+		ImageView pic = (ImageView) e.getSource();
+		pic.setImage(new Image(img.toURI().toString()));
+		
+	}
+	
+	@FXML
+	public void template1(ActionEvent e) {
+		pic2.setLayoutX(10);
+		pic2.setLayoutY(50);
+		pic2.setFitHeight(335);
+		pic2.setFitWidth(720);
+		pic1.setVisible(false);
+	}
+	
+	@FXML
+	public void template2(ActionEvent e) {
+		pic2.setLayoutX(10);
+		pic2.setLayoutY(50);
+		pic2.setFitWidth(355);
+		pic2.setFitHeight(335);
+		pic1.setVisible(true);
+		pic1.setLayoutX(367.5);
+		pic1.setLayoutY(50);
+		pic1.setFitWidth(355);
+		pic1.setFitHeight(335);
+	}
+
+	
 }
